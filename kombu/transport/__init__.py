27481f3eb10f937e08739baad7a0ab22948722a7
Lucas Li <lzl@uber.com>
2017-08-11 15:10:53 -0700

    add cherami transport

--- kombu/transport/__init__.py
+++ kombu/transport/__init__.py
@@ -69,6 +69,7 @@ TRANSPORT_ALIASES = {
     'zmq': 'kombu.transport.zmq:Transport',
     'amqplib': 'kombu.transport.amqplib:Transport',
     'qpid': 'kombu.transport.qpid:Transport',
+    'cherami': 'kombu.transport.cherami:Transport',
 }
 
 _transport_cache = {}
